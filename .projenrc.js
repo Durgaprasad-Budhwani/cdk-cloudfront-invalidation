de42fa69a6e8d17c03b775f4663e4e2d32afa821
Durgaprasad Budhwani <durgaprasad.budhwani@gmail.com>
2023-03-23 06:27:30 +1030

    🔧 chore(.projen/tasks.json): add MAJOR environment variable to release task 🔧 chore(.projenrc.js): remove majorVersion property and add it as an environment variable in release task

--- .projenrc.js
+++ .projenrc.js
@@ -11,6 +11,5 @@ const project = new awscdk.AwsCdkConstructLibrary({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
-  majorVersion: 0,
 });
 project.synth();
